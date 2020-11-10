a88745a9632a049f2d461a00ddaa92719ca78156
Jazz <mh216347@gmail.com>
2020-11-09 20:40:44 -0500

    Commit to add View, Controller and Main

--- src/main/View.java
+++ src/main/View.java
@@ -1,5 +1,18 @@
 package main;
 
-public class View {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/*
+ * View - class that will hold the GUI information and handle the visual display
+ * Based on MazeLabMVC Lab code
+ */
+public class View extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
---
+++ src/states/Empty.java
@@ -0,0 +1,17 @@
+package states;
+
+import javafx.scene.paint.Color;
+
+public class Empty extends ImmutableState{
+
+	public Empty() {
+		super();
+		cellColor = Color.BROWN;
+	}
+
+	@Override
+	public String getType() {
+		return this.EMPTY_CELL;
+	}
+
+}
