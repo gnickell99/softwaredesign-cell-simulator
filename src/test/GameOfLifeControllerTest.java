21c5d059efda882ab2b3398619f19809c7432910
Kim Jones <kjones77@elon.edu>
2020-11-11 13:26:13 -0500

    Made 2 test to check border edges

--- src/test/GameOfLifeControllerTest.java
+++ src/test/GameOfLifeControllerTest.java
@@ -14,19 +14,50 @@ import controller.GameOfLife;
 
 public class GameOfLifeControllerTest {
 	static GameOfLife golControl;
+	static int testGridSize = 4;
+	static int lastRow = 5;
 	
 	@BeforeClass
 	public static void getController() {
-		golControl = new GameOfLife(4,4); //Make object
+		golControl = new GameOfLife(testGridSize,testGridSize); //Make object
 		golControl.generateGrid(); //Make a new Grid
 		
 		//Manually make a testing scenario
-		golControl.grid[0][0].getType();
+		
+		
+		//Test prints:
+//		System.out.println(golControl.grid[5][0].getType());
+//		System.out.println(golControl.grid[0][0].getType());
+//		for(int i = 0; i < golControl.grid.length; i++) {
+//			System.out.println(golControl.grid[0][i].getType());
+//		}
+		
+		
+		
 	}
 
+	//Make sure the top row is all edge cells
 	@Test
-	public void edgeTest() {
-		fail("Not yet implemented");
+	public void topEdgeTest() {
+		String expected = "edge";
+		
+		//Loop through all cells in first row and make sure they are type edge
+		for(int i = 0; i < golControl.grid.length; i++) {
+			assertEquals(expected, golControl.grid[0][i].getType());
+		}
 	}
+	
+	//Make sure the bottom row is all edge cells
+	@Test
+	public void bottomEdgeTest() {
+		String expected = "edge";
+		
+		//Loop through all cells in last row and make sure they are type edge
+		for(int i = 0; i < golControl.grid.length; i++) {
+			assertEquals(expected, golControl.grid[lastRow][i].getType());
+		}
+	}
+	
+	
 
 }
