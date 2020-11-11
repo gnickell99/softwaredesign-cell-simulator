1fecb6fc9f494ae4d3be7c446f1363d42b884c6a
Camryn Williams <cwilliams65@elon.edu>
2020-11-11 17:59:48 -0500

    moved WildfireController to test package

---
+++ src/test/WildfireControllerTest.java
@@ -0,0 +1,45 @@
+package test;
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import main.*;
+import states.*;
+
+public class WildfireControllerTest {
+	static Wildfire wildfireControl;
+	//private State[][] controlBoard;
+	
+	@BeforeClass
+	public static void getController() {
+		wildfireControl = new Wildfire(4,4);
+		wildfireControl.setBurningTrees(2);
+		wildfireControl.generateGrid();
+		//controlBoard = 
+	}
+	
+	@Test
+	public void edgeTest() {
+		for(int i = 0; i < wildfireControl.board[0].length; i++) {
+			assert(wildfireControl.board[0][i].getType().equals("Edge"));
+		}
+		for(int i = 0; i < wildfireControl.board[0].length; i++) {
+			assert(wildfireControl.board[5][i].getType().equals("Edge"));
+		}
+	}
+	
+	@Test
+	public void burningTreeNumTest() {
+		int burningTreeCount = 0;
+		for(int i = 0; i < wildfireControl.board.length; i++) {
+			for(int j = 0; j < wildfireControl.board[0].length; j++) {
+				if(wildfireControl.board[i][j].getType().equals("BurningTree")) {
+					burningTreeCount++;
+				}
+			}
+		}
+		assert(burningTreeCount == wildfireControl.burningTrees);
+	}
+	
+	
+}
