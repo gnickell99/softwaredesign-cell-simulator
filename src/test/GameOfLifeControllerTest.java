ac48adda7c733bcd7d5e0f2101dfbea6447a280b
Kim Jones <kjones77@elon.edu>
2020-11-11 13:34:17 -0500

    Finished test for edge cells

--- src/test/GameOfLifeControllerTest.java
+++ src/test/GameOfLifeControllerTest.java
@@ -15,6 +15,7 @@ import controller.GameOfLife;
 public class GameOfLifeControllerTest {
 	static GameOfLife golControl;
 	static int testGridSize = 4;
+	static int firstRow = 0;
 	static int lastRow = 5;
 	
 	@BeforeClass
@@ -26,11 +27,11 @@ public class GameOfLifeControllerTest {
 		
 		
 		//Test prints:
-//		System.out.println(golControl.grid[5][0].getType());
+//		System.out.println(golControl.grid[0][1].getType());
 //		System.out.println(golControl.grid[0][0].getType());
-//		for(int i = 0; i < golControl.grid.length; i++) {
-//			System.out.println(golControl.grid[0][i].getType());
-//		}
+		for(int i = 0; i < golControl.grid.length; i++) {
+			System.out.println(golControl.grid[i][lastRow].getType());
+		}
 		
 		
 		
@@ -43,7 +44,7 @@ public class GameOfLifeControllerTest {
 		
 		//Loop through all cells in first row and make sure they are type edge
 		for(int i = 0; i < golControl.grid.length; i++) {
-			assertEquals(expected, golControl.grid[0][i].getType());
+			assertEquals(expected, golControl.grid[firstRow][i].getType());
 		}
 	}
 	
@@ -55,6 +56,28 @@ public class GameOfLifeControllerTest {
 		//Loop through all cells in last row and make sure they are type edge
 		for(int i = 0; i < golControl.grid.length; i++) {
 			assertEquals(expected, golControl.grid[lastRow][i].getType());
+		}
+	}
+	
+	//Make sure the left column is all edge cells
+	@Test
+	public void leftEdgeTest() {
+		String expected = "edge";
+		
+		//Loop through all cells in last row and make sure they are type edge
+		for(int i = 0; i < golControl.grid.length; i++) {
+			assertEquals(expected, golControl.grid[i][firstRow].getType());
+		}
+	}
+	
+	//Make sure the right column is all edge cells
+	@Test
+	public void rightEdgeTest() {
+		String expected = "edge";
+		
+		//Loop through all cells in last row and make sure they are type edge
+		for(int i = 0; i < golControl.grid.length; i++) {
+			assertEquals(expected, golControl.grid[i][lastRow].getType());
 		}
 	}
 	
