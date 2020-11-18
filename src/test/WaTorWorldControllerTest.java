d9df62ab3317323c30c7826f8f0eb53afa0a03cf
Kim Jones <kjones77@elon.edu>
2020-11-18 14:07:50 -0500

    designed test for all rules

--- src/test/WaTorWorldControllerTest.java
+++ src/test/WaTorWorldControllerTest.java
@@ -2,17 +2,19 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class WaTorWorldControllerTest {
 
-	@Test
-	public void test() {
+	@BeforeClass
+	public static void test() {
 		fail("Not yet implemented");
 	}
 
 	//Test for fish below:
 	
+	//Movement test
 	//Test Plan
 	//Test 1: Current cell should turn blue and fish should move to either: (2,1),(2,2),(1,2) (Color will be green)
 	//Fish at point (1,1) Water at point (2,1),(2,2),(1,2)
@@ -28,8 +30,24 @@ public class WaTorWorldControllerTest {
 		fail("Not yet implemented");
 	}
 	
+	//Breeding test
+	//Test 2: Current cell stay green and fish should give birth at either: (1,2), (2,1), (2,2) (Color will be green)
+	//Fish at point (1,1) Water at point (1,2), (2,1), (2,2)
+	@Test
+	public void fishTest3() {
+		fail("Not yet implemented");
+	}
+	
+	//Test 3: Current cell stay green and fish should give birth at either: (1,2) (Color will be green)
+	//Fish at point (1,1) Water at point (1,2) Edges at point (2,1), (2,2)
+	@Test
+	public void fishTest4() {
+		fail("Not yet implemented");
+	}
+	
 	//Test for sharks below:
 	
+	//Movement test
 	//Test 1: Current cell should turn blue and shark should move to either: (2,1),(2,2),(1,2) (Color will be yellow)
 	//shark at point (1,1) Water at point (2,1),(2,2),(1,2)
 	@Test
@@ -44,6 +62,7 @@ public class WaTorWorldControllerTest {
 		fail("Not yet implemented");
 	}
 	
+	//Eating test
 	//Test 3: Current cell should stay yellow and fish should turn blue 
 	//shark at point (1,1) Water at point (1,2),(2,2) fish at point (2,2)
 	@Test
@@ -55,6 +74,29 @@ public class WaTorWorldControllerTest {
 	//shark at point (1,1) Water at point (1,2) fish at point (2,2),(2,2)
 	@Test
 	public void sharkTest4() {
+		fail("Not yet implemented");
+	}
+	
+	//Breeding test
+	//Test 4: Current cell should stay yellow and should give birth at either: (2,1),(2,2),(1,2) (Color will be yellow)
+	//shark at point (1,1) Water at point (1,2), (2,1), (2,2)
+	@Test
+	public void sharkTest5() {
+		fail("Not yet implemented");
+	}
+	
+	//Test 5: Current cell should stay yellow and should give birth at either: (1,2) (Color will be yellow)
+	//shark at point (1,1) Water at point (1,2) edges at point (2,1), (2,2)
+	@Test
+	public void sharkTest6() {
+		fail("Not yet implemented");
+	}
+	
+	//Death test
+	//Test 4: Current cell should die (Color will be blue)
+	//shark at point (1,1) Water at point (1,2), (2,1), (2,2)
+	@Test
+	public void sharkTest7() {
 		fail("Not yet implemented");
 	}
 	
