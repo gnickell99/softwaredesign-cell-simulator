eaea26406ef606edb4129527352e44484fa20fcc
Kim Jones <kjones77@elon.edu>
2020-11-18 13:46:04 -0500

    Planned tests for rules 1 and 5

--- src/test/WaTorWorldControllerTest.java
+++ src/test/WaTorWorldControllerTest.java
@@ -11,9 +11,37 @@ public class WaTorWorldControllerTest {
 		fail("Not yet implemented");
 	}
 
-	//Test Plan
-	//Test 1: Cell should turn blue and fish should move to either: (2,1),(2,2),(1,2)
-	//Fish at point (1,1) Water at point ()
+	//Test for fish below:
 	
+	//Test Plan
+	//Test 1: Current cell should turn blue and fish should move to either: (2,1),(2,2),(1,2) (Color will be green)
+	//Fish at point (1,1) Water at point (2,1),(2,2),(1,2)
+	@Test
+	public void fishTest1() {
+		fail("Not yet implemented");
+	}
+	
+	//Test 2: Current cell should turn blue and fish should move to: (1,2) (Color will be green)
+	//Fish at point (1,1) Water at point (1,2) Edges at point (2,1), (2,2)
+	@Test
+	public void fishTest2() {
+		fail("Not yet implemented");
+	}
+	
+	//Test for sharks below:
+	
+	//Test 1: Current cell should turn blue and shark should move to either: (2,1),(2,2),(1,2) (Color will be yellow)
+	//shark at point (1,1) Water at point (2,1),(2,2),(1,2)
+	@Test
+	public void sharkTest1() {
+		fail("Not yet implemented");
+	}
+	
+	//Test 1: Current cell should turn blue and shark should move to either: (2,1),(2,2),(1,2) (Color will be yellow)
+	//shark at point (1,1) Water at point (1,2) Edges at point (2,1), (2,2)
+	@Test
+	public void sharkTest2() {
+		fail("Not yet implemented");
+	}
 	
 }
