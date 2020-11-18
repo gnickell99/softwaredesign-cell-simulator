dbdcb3df7f440a44f11f23f8567fb56121f97888
Kim Jones <kjones77@elon.edu>
2020-11-18 13:00:25 -0500

    Made the new test classes for 2 new sims

---
+++ src/test/WaTorWorldControllerTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WaTorWorldControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
