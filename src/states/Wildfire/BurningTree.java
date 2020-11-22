9da164a895d69ec8c1dab67b7971bed6ae62b477
gnickell99 <70291526+gnickell99@users.noreply.github.com>
2020-11-22 17:10:12 -0500

    Create BurningTree.java

---
+++ src/states/Wildfire/BurningTree.java
@@ -0,0 +1,36 @@
+package states.Wildfire;
+
+import java.util.List;
+
+//import controller.Controller;
+import javafx.scene.paint.Color;
+import states.State;
+
+/***
+ * 
+ * @author Grant Nickell
+ *
+ * The BurningTree Class is a state that inherits from MutableState,
+ * When found as a neighbor can spread the fire to a living tree,
+ * the burning tree itself will continue to burn until burn time runs out
+ *
+ */
+
+public class BurningTree extends WildFireMutables {
+	
+	public BurningTree(int burnTime) {
+		super(burnTime);
+		cellColor = Color.RED;
+	}
+
+	@Override
+	public State act(List<State> neighbors) {
+		if (burnTimer == 0) {
+			return new BurntDownTree();
+		}
+		this.burnTimer--;
+		return this;
+		
+	}
+
+}
